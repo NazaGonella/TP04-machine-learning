ad6e9debe08fd31aa36a3b5be6e5fdaea477bce2
Naza Gonella <nazagonella2@gmail.com>
2025-05-22 13:03:11 -0300

    checkpoint

--- src/models.py
+++ src/models.py
@@ -3,44 +3,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-class k_means():
+class KMeans():
     def __init__(self, X : np.ndarray, k : int):
-        self.k = k
-        self.X = X
+        self.k : int = k
+        self.X : np.ndarray = X
+        self.mu : np.ndarray = self.initialize_centroids()
     
     def initialize_centroids(self) -> np.ndarray:
         mu : np.ndarray = np.array([self.X[np.random.randint(len(self.X))] for _ in range(self.k)])
         return mu
     
-    def calculate_distances(self, mu : np.ndarray) -> np.ndarray:
+    def calculate_distances(self) -> np.ndarray:
         # forma vectorial de calcular la distancia de cada muestra al centroide
-        return np.linalg.norm(mu[:, np.newaxis] - self.X, axis=2)
+        return np.linalg.norm(self.mu[:, np.newaxis] - self.X, axis=2)
     
-    def fit_centroids(self, max_iterations : int = 250) -> np.ndarray:
-        mu : np.ndarray = self.initialize_centroids()
+    def fit_centroids(self, max_iterations : int = 250, runs : int = 1) -> np.ndarray:
         r : np.ndarray = np.zeros(shape=(self.X.shape[0], self.k))
-        for i in range(max_iterations):
-            r_old : np.ndarray = r.copy()
-            # for n in range(self.X.shape[0]):
-            #     k : int = np.argmin((np.linalg.norm(mu - self.X[n], axis=1)))
-            #     r[n] = np.zeros_like(range(self.k))
-            #     r[n][k] = 1
-            distancias : np.ndarray = self.calculate_distances(mu)
-            k = np.argmin(distancias, axis=0)
-            r[:] = np.zeros_like(r)
-            r[np.arange(len(k)), k] = 1
+        for t in range(runs):
+            for i in range(max_iterations):
+                r_old : np.ndarray = r.copy()
+                # for n in range(self.X.shape[0]):
+                #     k : int = np.argmin((np.linalg.norm(mu - self.X[n], axis=1)))
+                #     r[n] = np.zeros_like(range(self.k))
+                #     r[n][k] = 1
+                k = np.argmin(self.calculate_distances(), axis=0)
+                r[:] = np.zeros_like(r)
+                r[np.arange(self.X.shape[0]), k] = 1
 
-            # recalculo centroides
-            number_of_points_per_cluster = np.sum(r, axis=0)
-            for k in range(self.k):
-                if number_of_points_per_cluster[k] == 0:
-                    continue
-                mu[k] = np.sum(self.X[r[:, k] == 1], axis=0) / number_of_points_per_cluster[k]
-            # analizo si converge
-            if np.array_equal(r, r_old):
-                print("done at iteration ", i)
-                break
-        return
+                # recalculo centroides
+                number_of_points_per_cluster = np.sum(r, axis=0)
+                for k in range(self.k):
+                    if number_of_points_per_cluster[k] == 0:
+                        continue
+                    self.mu[k] = np.sum(self.X[r[:, k] == 1], axis=0) / number_of_points_per_cluster[k]
+                # veo si converge
+                if np.array_equal(r, r_old):
+                    print("done at iteration ", i)
+                    break
+        return self.mu
+    
+    def plot_clusters_2d(self):
+        k = np.argmin(self.calculate_distances(), axis=0)
+        plt.scatter(self.X[:, 0], self.X[:, 1], c=k, cmap='tab10', s=10, alpha=1)
+        plt.scatter(self.mu[:, 0], self.mu[:, 1], c='red', marker='x', s=50)
+        plt.title(f'K-Means (k = {self.k})')
+        plt.xlabel('A')
+        plt.ylabel('B')
+
 
 
 # project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
